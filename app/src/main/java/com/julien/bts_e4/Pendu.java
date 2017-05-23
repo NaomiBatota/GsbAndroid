324c0907e76864ab9de8d650594d74732788ea75
Julien Hivert <julienhivert.tt@gmail.com>
2017-05-23 13:57:44 +0200

    Ajout jeux mais bug de lancement

--- app/src/main/java/com/julien/bts_e4/Pendu.java
+++ app/src/main/java/com/julien/bts_e4/Pendu.java
@@ -1,18 +1,22 @@
 package com.julien.bts_e4;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Pendu extends Fragment {
-
+    private Button jeuPendu;
+    private Button jeupcc;
+    private View rootViews;
 
     public Pendu() {
         // Required empty public constructor
@@ -23,7 +27,28 @@ public class Pendu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pendu, container, false);
+        rootViews = inflater.inflate(R.layout.fragment_pendu, container, false);
+        Game();
+        return rootViews;
     }
 
+    public void Game(){
+        jeuPendu  =(Button) jeuPendu.findViewById(R.id.jeuPendu);
+        jeupcc = (Button) jeupcc.findViewById(R.id.jeupcc);
+        jeuPendu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),PenduActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        jeupcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(getActivity(), PccActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
